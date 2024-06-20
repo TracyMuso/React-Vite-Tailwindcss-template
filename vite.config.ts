e9d1afef2ae7190836ebd5a51c43b9bbd74cc532
TracyMuso <musongoletracy@gmail.com>
2024-06-20 12:28:15 +0200

    add testing libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable */
+
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./testConfig.ts'],
+		coverage: {
+			provider: 'v8',
+			exclude: [...configDefaults.coverage.exclude, '*.config.js'],
+			all: true,
+		},
+	},
+	plugins: [react()],
+	build: {
+		target: 'esnext',
+	},
+});
