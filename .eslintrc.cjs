df4ade29b2619b021ff775cb365b5d7ea162c391
TracyMuso <musongoletracy@gmail.com>
2024-06-20 12:12:46 +0200

    add eslint, prrettier and husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,12 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+   'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended', "plugin:react/recommended", "prettier"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
