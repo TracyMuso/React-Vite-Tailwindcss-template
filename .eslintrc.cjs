ce182cd4b4bcc1c008bd9add04f0edc59631a9fa
TracyMuso <musongoletracy@gmail.com>
2024-06-25 10:51:41 +0200

    update READme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,22 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-   'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended', "plugin:react/recommended", "prettier"
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prettier'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:react/recommended',
+		'prettier',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'prettier'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'@typescript-eslint/consistent-type-definitions': ['off'],
+		'@typescript-eslint/explicit-function-return-type': 'off',
+	},
+};
